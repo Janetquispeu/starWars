f05fbe08b4c369cf7c5b6106f23a236c03670e70
janetquispeu@gmail.com <janetquispeu@gmail.com>
2016-11-15 13:34:47 -0600

    heroku

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express=require("express");
 var app=express();
 
 app.use(express.static(__dirname + "/public"));
-app.listen(2010,function() {
+
+app.set("port",process.env.PORT || 2010);//hvhyu
+
+app.listen(app.get("port"),function() {
 	console.log("servidor encendido");
 });
